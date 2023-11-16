3843e2990213f0587ba34d4f196c6d2af9446f6c
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2023-11-16 21:19:21 +0200

    Створено найпростіший консольний додаток

---
+++ pract6 solution/pract6 project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract6_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
